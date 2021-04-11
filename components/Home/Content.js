140f42af0592e9ff298ee96d432902363a656f21
Mark <electormarK@gmail.com>
2021-04-11 20:33:38 +0700

    update ScrollView inCardHome

--- components/Home/Content.js
+++ components/Home/Content.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView } from 'react-native';
+import { StyleSheet, Text, ScrollView, SafeAreaView, View } from 'react-native';
 import Card from './Card';
 
 
-export default function Content() {
+const Content = () => {
     return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Card/>
-            <Card/>
-            <Card/>
-            <Card/>
+
+        <ScrollView style={styles.container} >
+            <View style={styles.ListItem}>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </View>
         </ScrollView>
+
+
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
+        paddingBottom:'50%',
+        
+        height:'50%',
+        
+    },
+    ListItem: {
+        flex: 1,
         justifyContent: 'center',
         flexDirection: 'row',
         flexWrap: 'wrap',
-        flexGrow: 1,
+        marginBottom:25,
         marginTop:10,
-        
-        
     }
-})
+});
+
+export default Content;
